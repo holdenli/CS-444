24ebaa570cdcc3eec992f3df43b173699bce760f
Holden Li <h55li@uwaterloo.ca>
2013-02-05 14:51:06 -0500

    Read all Java keywords as tokens.

---
+++ assignment_testcases/a1/H_NonJoosConstructs_Keywords.java
@@ -0,0 +1,9 @@
+// PARSER_WEEDER
+// JOOSW:PARSER_EXCEPTION
+public class H_NonJoosConstruct_Keywords {
+  public H_NonJoosConstruct_Keywords() {}
+  public static int test() {
+      boolean volatile = false;
+      return 123;
+  }
+}
