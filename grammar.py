565a7e6ca724fdaca68f02036d2ead564a26eb7f
Charlie Mei <cmei@uwaterloo.ca>
2013-02-06 16:06:13 -0500

    Remove two-sided recursion in TypeDeclarations

--- grammar.py
+++ grammar.py
@@ -127,7 +127,7 @@ RULES = {
     ],
     'TypeDeclarations': [
         ['TypeDeclaration'],
-        ['TypeDeclarations', 'TypeDeclarations'],
+        ['TypeDeclarations', 'TypeDeclaration'],
     ],
 
     # Package declarations (S7.4)
