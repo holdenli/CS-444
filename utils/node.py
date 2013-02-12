d4d579deede6807b78833d6baa664834163ef798
Holden Li <h55li@uwaterloo.ca>
2013-02-12 14:02:54 -0500

    Added iterators to node.py.

--- utils/node.py
+++ utils/node.py
@@ -9,21 +9,46 @@ class Node:
     token = None
     children = []
 
-    def __init__(self):
-        pass
+    def __init__(self, n=None, t=None, c=[]):
+        self.name = n
+        self.token = t
+        self.children = c
 
-    def first_token(self):
-        if self.token != None:
-            return self.token
-        elif self.children: # empty lists are false
-            return self.children[0].first_token()
-        else:
-            return None
+    def __repr__(self):
+        return "<Node: %s>" % (self.name)
+
+    def bfs_iter(self, leafs=False):
+        queue = [self]
+        while len(queue) > 0:
+            node = queue.pop(0)
+            queue += node.children
+            if leafs and len(node.children) != 0:
+                continue
+            yield node
+
+    def dfs_iter(self, leafs=False):
+        stack = [self]
+        while len(stack) > 0:
+            node = stack.pop(0)
+            stack = node.children + stack
+            if leafs and len(node.children) != 0:
+                continue
+            yield node
+
+    def leafs(self):
+        return list(self.dfs_iter(True))
 
 if __name__ == "__main__":
-    n = Node()
-    n2 = Node()
-    n2.token = "OMG"
-    n.children = [n2]
-    print(n.first_token())
+    n = Node("OMG1",
+        c=[
+            Node("1", c=[Node("A", c=[Node("C", ("token"))])]),
+            Node("2"),
+            Node("3", c=[Node("B", ("token"))])
+            ])
+    print("BFS")
+    print(list(n.bfs_iter()))
+    print(list(n.bfs_iter(True)))
+    print("DFS")
+    print(list(n.dfs_iter()))
+    print(list(n.dfs_iter(True)))
 
