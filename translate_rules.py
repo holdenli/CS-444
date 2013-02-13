79843c354560da070e84f14590772358e6bfc49e
Vardhan Mudunuru <vmudunur@uwaterloo.ca>
2013-02-12 21:12:09 -0500

    Temporary push with new grammar.
    To generate the python source w/ datastructure from the `rules` file, use:
    cat rules | ./translate_rules.py > newrules.py
    
    You must now change gen_parsetable.py to use newrules.py for its rules

---
+++ translate_rules.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import pprint
+import sys
+
+terms = []
+nonterm = ""
+
+g = {}
+
+for line in sys.stdin:
+    line = line.strip()
+
+    if line == '':
+        continue
+
+    if line[-1] == ':':
+        nonterm = line[:-1].strip()
+        g[nonterm] = []
+    else:
+        if line == '__EPSILON__':
+            g[nonterm].append([])
+        else:
+            g[nonterm].append(line.split())
+
+print 'START_SYMBOL="Goal"'
+print 'RULES = %s' % pprint.pformat(g)
