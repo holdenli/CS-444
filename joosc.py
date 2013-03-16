aa52912795c47df5df5d64ba2d2d0759b34f0701
Vardhan Mudunuru <vmudunur@uwaterloo.ca>
2013-03-15 23:47:03 -0400

    -s name option

--- joosc.py
+++ joosc.py
@@ -152,6 +152,14 @@ def joosc(targets, options):
         sys.exit(0)
 
     name_resolve.name_link(pkg_index, type_index, class_index)
+
+    if options.stage == 'name':
+        for i, _ in enumerate(ast_list): 
+            if options.include_stdlib == False or target_files[i] not in opts.stdlib_files or \
+                    options.print_stdlib == True:
+                ast_list[i].pprint()
+        sys.exit(0)
+
     #typecheck.typecheck(type_index, class_index)
 
 # INTERFACE
@@ -177,6 +185,7 @@ def parse_options(args=sys.argv):
         'weeder',
         'ast',
         'hierarchy',
+        'name',
         'end'
     ]
     parser.add_option('-s', '--stage', action='store', dest='stage',
