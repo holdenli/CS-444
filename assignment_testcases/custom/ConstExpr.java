768c36f62f1f9df19d30c0653067a90814faf2d0
Holden Li <h55li@uwaterloo.ca>
2013-03-19 00:30:45 -0400

    Const Expr testcase. Make sure this passes.

---
+++ assignment_testcases/custom/ConstExpr.java
@@ -0,0 +1,13 @@
+// Use with reachability to determine if we evaluate const expressions correctly
+// (JLS 15.28 Constant Expression)
+public class ConstExpr {
+
+    public ConstExpr() {
+        while((1+2-3*1/1) > -1) {}
+        while((1 >= 1) && (1 <= 1) || false) {}
+        while((2 > 1) && (0 < 1) || false) {}
+        while((int)1 % (short)1 != (char)50) {}
+        while(!true == false) {}
+        while("WHAT" == (String)"WHAT") {}
+    }
+}
