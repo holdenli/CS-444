172cc0ea18f6f33e007f5b343f9e3821b7d9f18a
Holden Li <h55li@uwaterloo.ca>
2013-04-05 15:54:13 -0400

    Test field hiding stuff

--- assignment_testcases/custom/Inherits2.java
+++ assignment_testcases/custom/Inherits2.java
@@ -2,8 +2,12 @@ import apple.*;
 
 public class Inherits2 extends Inherits {
 
-    public int i;
+    public int z1;
+    public int z2;
     public int j;
+    public int z;
+    public int i;
+    public int zzz;
 
     public Inherits2() {}
 
